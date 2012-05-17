1f4b93713f0fd8d9f791c196e3746412ca397c54
Adam Hoddinott <adam@nervosa>
2012-05-17 11:31:23 +1000

    Added ShopLogo model

--- apps/shops/models.py
+++ apps/shops/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from shopifyable.models import Shop
@@ -9,3 +11,18 @@ class Shop(Shop):
     A  Shop/Store
     """
     pass
+
+class ShopLogo(models.Model):
+    """
+    A logo for a shop (used for invoices)
+    """
+    def get_upload_path(instance, filename):
+        return os.path.join(
+            "shoplogos/%s_%d/" % (instance.shop.title, instance.shop.id), filename)
+
+    shop = models.OneToOneField(Shop, primary_key=True)
+    image = models.ImageField(upload_to=get_upload_path)
+
+    def __unicode__(self):
+        return u'%s' % self.shop
+
