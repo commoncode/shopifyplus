ab7b515b56d60c2f9f674f354b4d73803c802282
Daryl Antony <daryl@commoncode.com.au>
2012-07-18 09:40:06 +1000

    roles.py

---
+++ roles.py
@@ -0,0 +1,8 @@
+vhosts = {
+	# Shopify Plus
+	'shopifyplus.commoncode.com.au': {
+		'hosts': ['shopifyplus.commoncode.com.au'], # could be a callable returning a list of hosts
+		'manage': 'manage.py',
+		'settings': 'settings',
+	},
+}
