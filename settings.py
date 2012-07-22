2c753ee7f31c7c43d15bf90c3a57a976f04b5eef
Daryl Antony <daryl@commoncode.com.au>
2012-07-22 17:41:59 +1000

    adjust site_media dir

--- settings.py
+++ settings.py
@@ -57,7 +57,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "site_media", "media")
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "..", "site_media", "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -66,7 +66,7 @@ MEDIA_URL = "/site_media/media/"
 
 # Absolute path to the directory that holds static files like app media.
 # Example: "/home/media/media.lawrence.com/apps/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media", "static")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "..", "site_media", "static")
 
 # URL that handles the static files like app media.
 # Example: "http://media.lawrence.com"
