f3f181941dbe325a6f80c18fa9e29b06ef97886a
mark hellewell <mark.hellewell@icloud.com>
2013-02-11 16:39:34 +1100

    modified roles.py so celery known about during a deploy

--- roles.py
+++ roles.py
@@ -1,9 +1,10 @@
 vhosts = {
-	# Shopify Plus
-	'shopifyplus.commoncode.com.au': {
-		'hosts': ['shopifyplus.commoncode.com.au'], # could be a callable returning a list of hosts
-		'manage': './manage.py',
-		'settings': 'settings',
-		'requirements': 'requirements/project.txt',
-	},
+    # Shopify Plus
+    'shopifyplus.commoncode.com.au': {
+        'hosts': ['shopifyplus.commoncode.com.au'], # could be a callable returning a list of hosts
+        'manage': './manage.py',
+        'settings': 'settings',
+        'requirements': 'requirements/project.txt',
+        'celery': True,
+    },
 }
