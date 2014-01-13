9deb3beb3237242be2bd35f64c140f9ae7709d1a
Y3K <dboy.y3k@gmail.com>
2014-01-12 21:59:33 -0600

    Gram Fix WIP

--- apps/procurement/utils.py
+++ apps/procurement/utils.py
@@ -27,12 +27,12 @@ def procurement_item_defaults(queryset):
         pass
     else:
         for procurement_item in procurement_items:
-        
+
             try:
                 procurement_weight_price = float(1000.00) / float(procurement_item.product_variant.grams) * procurement_item.product_variant.price
             except:
                 procurement_weight_price = None
-            
+
             procurement_item.procurement_weight = procurement_item.order_weight
             procurement_item.procurement_quantity = procurement_item.order_quantity
             procurement_item.procurement_unit_weight = procurement_item.product_variant.grams
@@ -42,7 +42,7 @@ def procurement_item_defaults(queryset):
             procurement_item.procured_by = User.objects.get(pk=1)
             procurement_item.procured_at = timezone.now()
             procurement_item.save()
-            
+
         return procurement_items
 
 
@@ -74,40 +74,40 @@ def procurement_orders(queryset, allow_closed_orders=False):
             """
             Validation error is likely to be an Integrity Error
             due to the unique_together constraint on Procurement and Order
-            
+
             TODO: assemble an error msg, and skip over the order.
             """
         else:
             procurement_order.save()
             print "Saved procurement order:", procurement_order
-            
+
     # Retrieve the OrderItems for later use
     order_item_kwargs = {
         'order__in': queryset, }
-    order_items = OrderItem.objects.filter(**order_item_kwargs) 
-    
+    order_items = OrderItem.objects.filter(**order_item_kwargs)
+
     # Get the distinct set of product_variants found in order_items
     product_variant_kwargs = {
         'shopify_product_variant_id__in': order_items.values_list('shopify_product_variant_id', flat=True).distinct()}
     product_variants = ProductVariant.objects.filter(**product_variant_kwargs).distinct()
-    
+
     for product_variant in product_variants:
-        
+
         oli_kwargs = {
             'shopify_product_variant_id': product_variant.shopify_product_variant_id, }
-        
+
         # Assemble values for kwargs inclusion
         units = order_items.filter(**oli_kwargs).aggregate(Sum('quantity'))['quantity__sum']
-        grams = product_variant.grams
+        grams = product_variant.grams or 0
         order_weight = units * grams
-        
+
         procurement_item_kwargs = {
             'procurement': procurement,
             'product_variant': product_variant,
             'order_weight': order_weight,
             'order_units': units, }
         procurement_item = ProcurementItem(**procurement_item_kwargs)
-        
+
         try:
             procurement_item.full_clean()
         except ValidationError, e:
